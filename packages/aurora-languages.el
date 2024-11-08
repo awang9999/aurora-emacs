a2e7f7ac6fb2752456a77f42df3a0555a727863b
Alexander Wang <alexander.wang2001@gmail.com>
2024-11-08 10:19:50 -0800

    Added treesitter

--- packages/aurora-languages.el
+++ packages/aurora-languages.el
@@ -35,4 +35,12 @@
          (typescript-mode . eglot-ensure)
          (kotlin-mode . eglot-ensure)))
 
+;; Tree-sitter
+(use-package treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
 (provide 'aurora-languages)
