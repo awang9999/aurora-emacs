2195eb84c8684bffb73ad990c2ac180838e72d63
Alexander Wang <alexander.wang2001@gmail.com>
2024-11-12 21:20:38 -0800

    Added vertico-posframe to make minibuffer show up a la VSCode Command Palette

--- packages/aurora-vertico.el
+++ packages/aurora-vertico.el
@@ -2,4 +2,26 @@
   :init
   (vertico-mode))
 
+(defun posframe-poshandler-frame-top-center-offset (info)
+  "Aurora Custom Posframe position handler.
+
+This poshandler function let top edge center of posframe align
+to top edge center of frame and offset down by 48.
+
+The structure of INFO can be found in docstring of
+`posframe-show'."
+  (cons (/ (- (plist-get info :parent-frame-width)
+              (plist-get info :posframe-width))
+           2)
+        48))
+
+(use-package vertico-posframe
+  :init
+  (vertico-posframe-mode 1)
+  :config
+  (setq vertico-posframe-parameters
+      '((left-fringe . 4)
+        (right-fringe . 4)))
+  (setq vertico-posframe-poshandler 'posframe-poshandler-frame-top-center-offset))
+
 (provide 'aurora-vertico)
