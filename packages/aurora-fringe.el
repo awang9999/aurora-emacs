90f0334fefdcf44e2776cca2f7f6ab5414835558
Alexander Wang <alexander.wang2001@gmail.com>
2024-11-22 00:29:20 -0800

    modified git-gutter fringe colors

--- packages/aurora-fringe.el
+++ packages/aurora-fringe.el
@@ -1,12 +1,19 @@
 (use-package git-gutter
-  :hook ((prog-mode org-mode) . git-gutter-mode )
+  :hook ((prog-mode org-mode) . git-gutter-mode)
   :config
   (setq git-gutter:update-interval 0.02))
 
 (use-package git-gutter-fringe
   :config
-  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
-  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+  (define-fringe-bitmap 'git-gutter-fr:added [#b11111000] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [#b11111000] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted
+  [#b11111111
+   #b11111111
+   #b11111111
+   #b11111111] nil nil 'bottom)
+  (set-face-foreground 'git-gutter-fr:added "pale green")
+  (set-face-foreground 'git-gutter-fr:modified "light coral")
+  (set-face-foreground 'git-gutter-fr:deleted "slate gray"))
 
 (provide 'aurora-fringe)
